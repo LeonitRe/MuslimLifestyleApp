cbf2c646ae3866ae7a8913ae2e849e98ddc8b5f1
EdiMujota <eduardmujota@gmail.com>
2023-05-05 23:00:04 +0200

    Created router-link for login and register view

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -31,6 +31,22 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/ContactusView.vue')
+    },
+    {
+      path: '/Login',
+      name: 'Login',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/user/LoginView.vue')
+    },
+    {
+      path: '/Register',
+      name: 'Register',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/user/RegisterView.vue')
     }
   ]
 })
